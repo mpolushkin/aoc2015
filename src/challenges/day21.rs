5f1deddb428cc515b8461a1346367f5931ef1fdc
Michael Polushkin <m.polushkin@gmail.com>
2023-06-04 12:40:51 +0200

    Day 21 player inventory

--- src/challenges/day21.rs
+++ src/challenges/day21.rs
@@ -1,5 +1,55 @@
 use std::{collections::HashMap, str::FromStr};
 
+enum ItemKind {
+    Weapon,
+    Armor,
+    Ring,
+}
+
+struct Item {
+    kind: ItemKind,
+    name: &'static str,
+    cost: u32,
+    damage: u32,
+    armor: u32,
+}
+
+struct Player {
+    hit_points: u32,
+    inventory: Vec<Item>,
+}
+
+impl Player {
+    fn new(hit_points: u32) -> Self {
+        Self {
+            hit_points,
+            inventory: Vec::new(),
+        }
+    }
+
+    fn set_inventory(&mut self, inventory: Vec<Item>) {
+        self.inventory = inventory;
+    }
+
+    fn inventory_cost(&self) -> u32 {
+        self.inventory.iter().map(|item| item.cost).sum()
+    }
+}
+
+impl Fighter for Player {
+    fn hit_points(&self) -> u32 {
+        self.hit_points
+    }
+
+    fn damage(&self) -> u32 {
+        self.inventory.iter().map(|item| item.damage).sum()
+    }
+
+    fn armor(&self) -> u32 {
+        self.inventory.iter().map(|item| item.armor).sum()
+    }
+}
+
 #[derive(Debug, PartialEq, Eq)]
 struct Boss {
     hit_points: u32,
@@ -43,6 +93,12 @@ impl FromStr for Boss {
     }
 }
 
+trait Fighter {
+    fn hit_points(&self) -> u32;
+    fn damage(&self) -> u32;
+    fn armor(&self) -> u32;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -63,4 +119,40 @@ mod tests {
             })
         );
     }
+
+    #[test]
+    fn player_inventory() {
+        let mut player = Player::new(42);
+        player.set_inventory(vec![
+            Item {
+                kind: ItemKind::Weapon,
+                name: "dagger",
+                cost: 5,
+                damage: 6,
+                armor: 0,
+            },
+            Item {
+                kind: ItemKind::Armor,
+                name: "chainmail",
+                cost: 7,
+                damage: 0,
+                armor: 3,
+            },
+            Item {
+                kind: ItemKind::Ring,
+                name: "crazy ring",
+                cost: 100,
+                damage: 12,
+                armor: 8,
+            },
+        ]);
+
+        assert_eq!(player.inventory_cost(), 112);
+
+        let fighter = &player as &dyn Fighter;
+        assert_eq!(fighter.hit_points(), 42);
+        assert_eq!(fighter.damage(), 18);
+        assert_eq!(fighter.armor(), 11);
+    }
+
 }
